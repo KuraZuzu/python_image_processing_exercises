1318c21494eb63735d3d446cb21efa0bac5dfe41
kurasawa <kurazuzukun3@gmail.com>
2024-10-13 16:46:23 +0900

    YOLOXでのフルスクリーン表示を完成させた

--- yolox_inference.py
+++ yolox_inference.py
@@ -6,56 +6,38 @@ from yolox.data.data_augment import preproc
 from yolox.exp import get_exp
 from yolox.utils import postprocess, vis
 
-## 画像サイズ関連
+# YOLOXカスタムクラス
+CUSTOM_CLASSES = ["damage_panel"]
+
 # 表示画像
-DISPLAY_IMAGE_W = 1920
-DISPLAY_IMAGE_H = 1080
+FULLSCREEN_W = 1920
+FULLSCREEN_H = 1080
+
 # 撮影画像
 CAPTURE_IMAGE_W = 1280
 CAPTURE_IMAGE_H = 720
+
 # 推論画像
 PREDICT_IMAGE_W = 1280
 PREDICT_IMAGE_H = 704
 
-# 推論画像から表示画像にリサイズする係数
-# PREDICT_TO_DISPLAY_IMAGE_RATIO_W = DISPLAY_IMAGE_W / PREDICT_IMAGE_W
-# PREDICT_TO_DISPLAY_IMAGE_RATIO_H = DISPLAY_IMAGE_H / PREDICT_IMAGE_H
-# print(f"w:{PREDICT_TO_DISPLAY_IMAGE_RATIO_W}  h:{PREDICT_TO_DISPLAY_IMAGE_RATIO_H}")
+SCALE_WIDTH = FULLSCREEN_W / CAPTURE_IMAGE_W
+SCALE_HEIGHT = FULLSCREEN_H / CAPTURE_IMAGE_H
+SCALE = min(SCALE_WIDTH, SCALE_HEIGHT)
+FULLSCREEN_IMAGE_WIDTH = int(CAPTURE_IMAGE_W * SCALE)
+FULLSCREEN_IMAGE_HEIGHT = int(CAPTURE_IMAGE_H * SCALE)
+IMAGE_INIT_X = (FULLSCREEN_W - FULLSCREEN_IMAGE_WIDTH) // 2
+IMAGE_INIT_Y = (FULLSCREEN_H - FULLSCREEN_IMAGE_HEIGHT) // 2
 
-# 撮影画像から表示画像にリサイズする係数
-# CAPTURE_TO_DISPLAY_IMAGE_RATIO_W = DISPLAY_IMAGE_W / CAPTURE_IMAGE_W
-# CAPTURE_TO_DISPLAY_IMAGE_RATIO_H = DISPLAY_IMAGE_H / CAPTURE_IMAGE_H
+cv2.namedWindow("yolox_detection", cv2.WINDOW_NORMAL)
+cv2.setWindowProperty(
+    "yolox_detection", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-# 推論画像から撮影画像にリサイズする係数
-PREDICT_TO_CAPTURE_IMAGE_RATIO_W = CAPTURE_IMAGE_W / PREDICT_IMAGE_W
-PREDICT_TO_CAPTURE_IMAGE_RATIO_H = CAPTURE_IMAGE_H / PREDICT_IMAGE_H
+# キャンバスを準備
+canvas = np.zeros((FULLSCREEN_H, FULLSCREEN_W, 3), dtype="uint8")
 
-# カスタムクラス名
-CUSTOM_CLASSES = ["damage_panel"]
-
-
-def imshow_fullscreen(winname, img):
-    monitor = get_monitors()[0]
-    screen_width = monitor.width
-    screen_height = monitor.height
-
-    img_height, img_width = img.shape[:2]
-    scale_width = screen_width / img_width
-    scale_height = screen_height / img_height
-    scale = min(scale_width, scale_height)
-    window_width = int(img_width * scale)
-    window_height = int(img_height * scale)
-    img_resized = cv2.resize(img, (window_width, window_height), interpolation=cv2.INTER_AREA)
-
-    canvas = np.zeros((screen_height, screen_width, 3), dtype="uint8")
-    x = (screen_width - window_width) // 2
-    y = (screen_height - window_height) // 2
-    canvas[y:y+window_height, x:x+window_width] = img_resized
-
-    cv2.namedWindow(winname, cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty(winname, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    cv2.imshow(winname, canvas)
-
+# 表示するモニターを確保
+monitor = get_monitors()[0]
 
 # YOLOXのモデル設定とロード
 exp = get_exp(None, "yolox-s")
@@ -64,7 +46,8 @@ model = exp.get_model()
 model.eval()
 
 # 重みのロード
-ckpt = torch.load("./../YOLOX/models/4_0920_1000pic_best.pth", map_location="cuda")
+ckpt = torch.load("./../YOLOX/models/4_0920_1000pic_best.pth",
+                  map_location="cuda")
 model.load_state_dict(ckpt["model"])
 
 # モデル全体をGPUにロード
@@ -85,12 +68,14 @@ while True:
     # 前処理（リサイズ）
     img, ratio = preproc(frame, (PREDICT_IMAGE_H, PREDICT_IMAGE_W))
     img = torch.from_numpy(img).unsqueeze(0).float().to("cuda")
+
     # 推論
     with torch.no_grad():
         outputs = model(img)
-        outputs = postprocess(outputs, exp.num_classes, 0.8, exp.nmsthre, class_agnostic=True)
+        outputs = postprocess(outputs, exp.num_classes,
+                              0.8, exp.nmsthre, class_agnostic=True)
 
-    # 結果の表示
+    # 推論結果を描画
     if outputs[0] is not None:
         bboxes = outputs[0][:, 0:4] / ratio
         cls = outputs[0][:, 6]
@@ -99,9 +84,17 @@ while True:
     else:
         vis_res = frame
 
-    # 結果を表示
+    # 1. 結果を表示（オリジナル解像度のまま）
     # cv2.imshow("YOLOX Detection", vis_res)
-    imshow_fullscreen("YOLOX Detection", vis_res)
+
+    # 2. 結果を表示（フルスクリーン解像度）
+    img_resized = cv2.resize(vis_res, (FULLSCREEN_IMAGE_WIDTH,
+                             FULLSCREEN_IMAGE_HEIGHT), interpolation=cv2.INTER_AREA)
+    canvas[IMAGE_INIT_Y:IMAGE_INIT_Y+FULLSCREEN_IMAGE_HEIGHT,
+           IMAGE_INIT_X:IMAGE_INIT_X+FULLSCREEN_IMAGE_WIDTH] = img_resized
+    cv2.imshow("yolox_detection", canvas)
+
+    # ループ終了処理（"q"キー押して終了）
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
